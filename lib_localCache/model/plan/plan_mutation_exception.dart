f7b840627e1e23e254e7620bcf228bd612d48095
LeeHgg <leeggang12@gmail.com>
2026-07-28 21:50:28 +0900

    Remove unused folders

--- lib_localCache/model/plan/plan_mutation_exception.dart
+++
@@ -1,9 +0,0 @@
-/// Domain-specific exception thrown when a plan mutation violates rules.
-class PlanMutationException implements Exception {
-  PlanMutationException(this.message);
-
-  final String message;
-
-  @override
-  String toString() => 'PlanMutationException($message)';
-}
